f57a027054212307f3175427b08c623581408c6e
midastung <midas112800@gmail.com>
2021-06-26 22:32:25 +0800

    exercise

---
+++ Ch-13/vue-lesson/src/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="todo-item"> 
+        {{data}}
+    </li>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+}
+</script>
+
+<style lang="">
+    .todo-item{
+        font-size: 1.5em;
+        font-family: Arial, Helvetica, sans-serif;
+        background-color: #fed;
+        margin: 6px 0;
+        padding: 4px 8px;
+        cursor: pointer;
+    }
+
+    .todo-item:hover{
+        background-color: #def;
+    }
+</style>
