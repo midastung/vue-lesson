e0c070716449b8d31aafa9f13f861080fd0c3a4f
midastung <midas112800@gmail.com>
2021-07-01 17:27:38 +0800

    exercise

---
+++ Exercise/vue-router/src/Courses.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <div>
+        <h1>Courses</h1>
+        <router-view/>
+  </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
